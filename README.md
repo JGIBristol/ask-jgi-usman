2873a4a2f7c13ccaf1615683cf1cd7f6cbb6acbb
Natalie Thurlby <Natalie.thurlby@bristol.ac.uk>
2020-10-20 14:56:43 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,7 @@
+This repository contains two Python scripts:
+    1. `tidy_data.py`, tidies data sent by Usman into a format without repeated columns.
+    2. `pca.py`, performs a PCA and visualises the results, on the tidied data.
+
+These scripts were written as part of the [Jean Golding Institute](https://www.bristol.ac.uk/golding/)'s [Ask-JGI Service](https://www.bristol.ac.uk/golding/ask-jgi/).
+Contact [ask-jgi@bristol.ac.uk](mailto:ask-jgi@bristol.ac.uk) with any queries.
+
---
+++ scripts/pca.py
@@ -0,0 +1,34 @@
+"""
+This script:
+ - loads in our tidy data (created using `tidy_data.py`)
+ - transposes it (PCA module assumes columns = dimensions to reduce)
+ - performs PCA
+    - checks and prints variance explained
+    - transforms data into space of 2 principal components
+ - creates an interactive plot using plot.ly
+"""
+
+import pandas as pd  # for data manipulation
+import sklearn.decomposition.pca as pca  # for PCA
+import plotly.express as px  # for interactive plots
+
+# READ IN DATA:
+df = pd.read_csv('tidy_data.csv', index_col='wvn')
+
+# PCA:
+n_components = 2  # Number of principal components to reduce to.
+df = df.transpose()
+pca = pca.PCA(n_components=n_components)
+fit = pca.fit(df)
+transformed = pca.transform(df)
+
+# Variance explained:
+print(sum(pca.explained_variance_))
+
+# Plot figure:
+fig = px.scatter(x=transformed[:, 0],
+                 y=transformed[:, 1],
+                 text=list(df.index),
+                 labels={'x': 'Principal component 1', 'y': 'Principal component 2'})
+fig.update_traces(mode="markers")  # this line means text is displayed on hover, not all the time.
+fig.show()
